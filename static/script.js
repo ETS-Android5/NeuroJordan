571ca6010042ffa6d3e64a04b3078aae685b3f4a
Aleksandr887 <a.klyushnichenko@csc.sibsutis.ru>
2022-04-08 22:27:18 +0700

    some layout

---
+++ static/script.js
@@ -0,0 +1,36 @@
+async function load() {
+    const model = await tf.loadLayersModel('Model_js2/model.json');
+    //alert("model loaded");
+    return model;
+};
+
+function predict() {
+    model.then(model => {
+        //let selectedFile = document.getElementById("filePic").value
+        //alert(document.getElementById("filePic").value);
+        var img1 = new Image();
+        img1 = output;
+        //alert(img1.src);
+
+        let tensor = tf.browser.fromPixels(img1).resizeNearestNeighbor([224, 224]).toFloat()
+        let offset = tf.scalar(127.5)
+        let batched = tensor.sub(offset).reverse(2).expandDims()
+        const res = model.predict(batched)
+        document.getElementById("out_res").innerHTML = res;
+        alert(model.predict(batched));
+    });
+};
+
+const model = load();  // load model immediately to avoid delay when user clicks 'Predict'
+
+var loadFile = function(event) {
+var output = document.getElementById('output');
+output.src = URL.createObjectURL(event.target.files[0]);
+output.onload = function() {
+    
+URL.revokeObjectURL(output.src) // free memory
+document.getElementById("output").style.visibility = "visible";
+}
+
+}
+
