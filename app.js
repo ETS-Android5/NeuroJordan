9a35ba2ce851c089b8504e37bac439d83f0eccb6
Aleksandr887 <a.klyushnichenko@csc.sibsutis.ru>
2022-04-09 19:40:07 +0700

    Button + output locate center

--- app.js
+++ app.js
@@ -5,24 +5,7 @@ let app = express();
 
 const PORT = process.env.PORT || 80
 
-//app.use(express.static(__dirname + "/static"));
-
-//app.use("static/index.html");
-
-/*app.use(function(req, res, next) {
-   console.log(`${new Date()} - ${req.method} request for ${req.url}`);
-   next();
-});*/
-/*app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/static/index.html');
-    res.sendFile(__dirname + '/static/Model_js2/model.json');
-});*/
-
 app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.listen(PORT, function() {
     console.log(PORT);
